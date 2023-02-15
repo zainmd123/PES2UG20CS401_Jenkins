ef4fe08ad555c4442bbf4fa58619cedd3a248f76
zainmd123 <73015246+zainmd123@users.noreply.github.com>
2023-02-15 19:39:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run. I have also changed this code\n";
+  cout << "I have successfully built and run. I have also changed this code twice\n";
   return 0;
 }
